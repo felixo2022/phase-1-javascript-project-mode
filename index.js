81aec955c039c37c9d6406a3caac2f50f5ff35ab
felixo2022 <walterbarax@gmail.com>
2022-09-06 11:40:47 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,24 +1,54 @@
 let weather = {
-    "apikey": "f38ac0cb493bbed636a6fce66516be7b",
-    fetchWeather: function (city){
-        fetch(
-            ""
-            + city
-            + "&units=metric&appid="
-            +this.apikey
-        )
-        .then((response) => response.json())
-        .then((data) => console.log(data));
-
-
+    apiKey: "e89b90c9b992fec8c9378500c0e7b870",
+    fetchWeather: function (city) {
+      fetch(
+        "https://api.openweathermap.org/data/2.5/weather?q=" +
+          city +
+          "&units=metric&appid=" +
+          this.apiKey
+      )
+        .then((response) => {
+          if (!response.ok) {
+            alert("No weather found.");
+            throw new Error("No weather found.");
+          }
+          return response.json();
+        })
+        .then((data) => this.displayWeather(data));
     },
-    displayWeather: function(data) {
-        const { name } = data;
-        const { icon, description } = data.weather;
-        const { temp, humidity } = data.main;
-        const { speed } = data.wind;
-        console.log(name, icon, description, temp, humidity, wind);
-        document.querySelector(".city").innerText = "weather in" + name;
-        document.qurrySelector(".icon").src =
-    }
-}
+    displayWeather: function (data) {
+      const { name } = data;
+      const { icon, description } = data.weather[0];
+      const { temp, humidity } = data.main;
+      const { speed } = data.wind;
+      document.querySelector(".city").innerText = "Weather in " + name;
+      document.querySelector(".icon").src =
+        "https://openweathermap.org/img/wn/" + icon + ".png";
+      document.querySelector(".description").innerText = description;
+      document.querySelector(".temp").innerText = temp + "°C";
+      document.querySelector(".humidity").innerText =
+        "Humidity: " + humidity + "%";
+      document.querySelector(".wind").innerText =
+        "Wind speed: " + speed + " km/h";
+      document.querySelector(".weather").classList.remove("loading");
+      document.body.style.backgroundImage =
+        "url('https://source.unsplash.com/1600x900/?" + name + "')";
+    },
+    search: function () {
+      this.fetchWeather(document.querySelector(".search-bar").value);
+    },
+  };
+  
+  document.querySelector(".search button").addEventListener("click", function () {
+    weather.search();
+  });
+  
+  document
+    .querySelector(".search-bar")
+    .addEventListener("keyup", function (event) {
+      if (event.key == "Enter") {
+        weather.search();
+      }
+    });
+  
+  weather.fetchWeather("Denver");
